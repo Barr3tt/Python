74818885d141a8e8b4166b20bc3d2b0ebfff81c1
Barrett <williamsonbarrett@gmail.com>
2021-12-27 11:17:47 -0500

    Broken Switch Case

--- Lol.py
+++ Lol.py
@@ -2,19 +2,25 @@ import tkinter as tk
  
 class LolApp(tk.Frame):
     def __init__(self, master):
-        tk.Frame.__init__(self,master,width=300,height=200)
+        tk.Frame.__init__(self,master,width=400,height=400)
         self.master.title('Better League Client')
         self.pack_propagate(0)
         self.pack()
         self.choice_var = tk.StringVar()
-        self.choice = tk.OptionMenu(self,self.choice_var,'League Of Legends','LOL V2','Make League Better"')
+        self.choice = tk.OptionMenu(self,self.choice_var,'League Of Legends','LOL V2','Make League Better')
         self.choice_var.set('League Of Legends')
-        self.go_button = tk.Button(self,text='Go',command=self.print_out)
+        self.go_button = tk.Button(self,text='Go',command=self.action_out)
         self.go_button.pack(fill=tk.X, side=tk.BOTTOM)
         self.choice.pack(fill=tk.X, side=tk.BOTTOM)
- 
-    def print_out(self):
-        print('%s!' % (self.choice_var.get().title()))
+
+    def action_out(self):
+        if(self.choice_var.get().title()=="LOL V2"):
+            print('LOL V2')
+        elif(self.choice_var.get().title()=="Make League Better"):
+            print('Make League Better')
+        else:
+            print('League of Legends')
+
     def run(self):
         self.mainloop()
  
