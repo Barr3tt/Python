67cba2c7c34d9cc351297a381d9a6f3ea48bc26e
Barrett Williamson <williamsonbarrett@gmail.com>
2021-12-28 17:22:07 -0500

    Add Error Checking and Add picture

--- Lol Script/Lol.py
+++ Lol Script/Lol.py
@@ -1,5 +1,7 @@
 import tkinter as tk
-import webbrowser, os, signal
+import webbrowser, os, signal, tkinter.messagebox, io
+from PIL import Image, ImageTk
+from urllib.request import urlopen
 
 class LolApp(tk.Frame):
     def __init__(self, master):
@@ -7,16 +9,23 @@ class LolApp(tk.Frame):
         self.master.title('Better League Client')
         self.pack_propagate(0)
         self.pack()
+        self.mypage=urlopen('https://i.ytimg.com/vi/EEWYsOFKFIM/hqdefault.jpg')
+        self.tk_img =ImageTk.PhotoImage(Image.open(io.BytesIO(self.mypage.read())))
+        self.pic = tk.Label(self, image=self.tk_img)
         self.choice_var = tk.StringVar()
         self.choice = tk.OptionMenu(self,self.choice_var,'League Of Legends','The Better League','Make League Better')
         self.choice_var.set('Select Option')
         self.go_button = tk.Button(self,text='Go',command=self.action_out)
         self.go_button.pack(fill=tk.X, side=tk.BOTTOM)
         self.choice.pack(fill=tk.X, side=tk.BOTTOM)
+        self.pic.pack(fill=tk.X, side=tk.TOP)
 
     def action_out(self):
         if(self.choice_var.get().title()=="League Of Legends"):
-            os.startfile (r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Riot Games\League of Legends.lnk")
+            if (os.path.exists('C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Riot Games\League of Legends.lnk')):
+                os.startfile (r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Riot Games\League of Legends.lnk")
+            else:
+                tk.messagebox.showinfo('Error','You need to install League to Play it...')
         elif(self.choice_var.get().title()=="The Better League"):
             webbrowser.open("https://store.steampowered.com/app/1731720/FURRY_SEX_Cabaret/", new=2)
         elif(self.choice_var.get().title()=="Make League Better"):
